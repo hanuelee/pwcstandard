d3c1c00ffa81cd9592bf851958293986b248a0c2
Ha Nuel Lee <hanuelee@vt.edu>
2016-06-22 08:29:00 -0700

    Initial commit

---
+++ js/pwc-script.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $("[data-toggle='popover']").popover();
+});
+$(window).on("resize", function () {
+    var currSrc = $(".pwc-navbar-big .pwc-logo-image").attr("src");
+    if ($(this).width() <= 767 && $(".pwc-navbar-big .pwc-logo-image").length) {
+        $(".pwc-navbar-big .pwc-logo-image").attr({
+            "src": currSrc.substring(0, currSrc.lastIndexOf('/')) + "/onecolor-seal-95-web-white.png",
+            "height": 95
+        });
+    }
+    else if ($(this).width() > 767 && $(".pwc-navbar-big .pwc-logo-image").length) {
+        $(".pwc-navbar-big .pwc-logo-image").attr({
+            "src": currSrc.substring(0, currSrc.lastIndexOf('/')) + "/color-seal-95-web.png",
+            "height": 95
+        });
+    }
+});
+$("[data-toggle=offcanvas]").click(function () {
+    //$(this).closest(".pwc-offcanvas-nav-wrapper").toggleClass("active");
+    $(".pwc-offcanvas-content-wrapper, .pwc-offcanvas-nav-wrapper").toggleClass("active");
+});
+$(".pwc-accordion .panel-collapse").on("shown.bs.collapse", function () {
+    $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+});
+$(".pwc-accordion .panel-collapse").on("hidden.bs.collapse", function () {
+    $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
+});
---
+++ js/script.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $(".example-tabs").tab();
+    $("[data-toggle='popover']").popover();
+    setSidebarWidth();
+});
+$("a[href='#']").click(function (e) {
+    e.preventDefault();
+});
+$('a[href*="#"]:not([href="#"])').click(function () { /* For smooth scrolling */
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 750);
+            return false;
+        }
+    }
+});
+/**
+* Sets sidebar width
+*/
+function setSidebarWidth() {
+    var sidebarWidth = 0;
+    $("#sidebar").children().each(function () {
+        if ($(this).outerWidth() > sidebarWidth) {
+            sidebarWidth = $(this).outerWidth();
+        }
+    });
+    $("#sidebar").css({ "width": sidebarWidth });
+}
